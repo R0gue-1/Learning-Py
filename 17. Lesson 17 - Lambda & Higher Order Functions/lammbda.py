223a4706d8e6f4f33b216b7e081993177b9a6bde
Atibila <atibsis@gmail.com>
2024-10-21 21:40:52 +0000

    practice python code for python lambda functions

--- 17. Lesson 17 - Lambda & Higher Order Functions/lammbda.py
+++ 17. Lesson 17 - Lambda & Higher Order Functions/lammbda.py
@@ -14,4 +14,14 @@ print(sum(10,8))
 print("")
 
 #########################################
- 
+
+def funcBuilder(x):
+    return lambda num : num + x
+    
+addTen = funcBuilder(10)
+addTwenty = funcBuilder(20)
+
+print(addTen(7))
+print("")
+
+print(addTwenty(7))
