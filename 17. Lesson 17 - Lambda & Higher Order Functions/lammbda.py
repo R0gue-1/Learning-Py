6264ca0cb8d913dee1bdfaac14d89fdcb1810b3a
Atibila <atibsis@gmail.com>
2024-10-21 23:45:20 +0000

    higher order functions practice code for python

--- 17. Lesson 17 - Lambda & Higher Order Functions/lammbda.py
+++ 17. Lesson 17 - Lambda & Higher Order Functions/lammbda.py
@@ -25,3 +25,21 @@ print(addTen(7))
 print("")
 
 print(addTwenty(7))
+
+##########################################
+# Higher Order Functions map()
+
+numbers = [3, 7, 12, 18, 20, 21]
+
+squared_nums = map(lambda num : num * num, numbers)
+
+print(list(squared_nums))
+print("")
+
+##########################################
+# Higher Order Functions filter()
+
+odd_nums = filter(lambda num: num % 2 != 0, numbers)
+
+print(list(odd_nums))
+print("")
