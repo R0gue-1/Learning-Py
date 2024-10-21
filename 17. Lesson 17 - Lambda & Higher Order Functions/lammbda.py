746a45c70ba90891f31cbc89ea56cf81438eb294
Atibila <atibsis@gmail.com>
2024-10-21 21:35:23 +0000

    first practice code for lambda functions

---
+++ 17. Lesson 17 - Lambda & Higher Order Functions/lammbda.py
@@ -0,0 +1,17 @@
+squared = lambda num : num * num
+
+print(squared(2))
+print("")
+
+addTwo = lambda num : 2 + num
+
+print(addTwo(12))
+print("")
+
+sum = lambda a, b : a + b
+
+print(sum(10,8))
+print("")
+
+#########################################
+ 
