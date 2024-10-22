5428c27610423443dd231a76d7a41ceab34c78bd
Atibila <atibsis@gmail.com>
2024-10-22 14:36:21 +0100

    python practice code for classes that inherit other classes and the double init method

--- 18. Lesson 18 - Objects & Classes/classes.py
+++ 18. Lesson 18 - Objects & Classes/classes.py
@@ -25,4 +25,44 @@ print("")
 
 your_car = Vehicle('Cadillac', 'Escalade')
 your_car.get_make_model()
-your_car.moves();
+your_car.moves()
+
+#more classes
+
+class Airplane(Vehicle):
+    def __init__(self, make, model,faa_id):
+        # wont need this because its inherited
+        # self.make = make
+        # self.model = model
+
+        super().__init__(make, model)
+        self.faa_id = faa_id
+
+        
+
+    def moves(self):
+        print("Flies along.")
+
+class Truck(Vehicle):
+    def moves(self):
+        print("Rumbles along.")
+
+class GolfCart(Vehicle):
+    pass
+
+cesna = Airplane("Cesna", "Skyhawk", "FA-12345")
+mack = Truck("Caterpillar", "DAF")
+cesna = Airplane("Yamaha", "DC100")
+
+cesna.get_make_model()
+cesna.moves()
+print("")
+
+mack.get_make_model()
+mack.moves()
+print("")
+
+GolfCart.get_make_model()
+GolfCart.moves()
+print("")
+        
